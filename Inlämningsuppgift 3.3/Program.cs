6f9dc20422eb5733e0c62eeb577a7ebea62a9948
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-10-04 11:03:27 +0200

    Add project files.

---
+++ Inlämningsuppgift 3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uppgift_3._3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int svar = int.Parse(Console.ReadLine());
+            int pris = (svar * 80);
+            if (pris >= 950)
+            {
+                Console.WriteLine("Det kommer kosta 950 kronor att hyra bilen");
+            }
+            else
+            {
+                Console.WriteLine("Det kommer kosta " + pris + " kronor att hyra bilen");
+            }
+            Console.Read();
+        }
+    }
+}
